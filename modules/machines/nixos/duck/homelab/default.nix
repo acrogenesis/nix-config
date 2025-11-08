b51758ea1f18eb48621631a6e2e9658bd777b726
Adrian Rangel <adrian.rangel@gmail.com>
2025-11-08 01:26:25 -0600

    update

--- modules/machines/nixos/duck/homelab/default.nix
+++ modules/machines/nixos/duck/homelab/default.nix
@@ -12,7 +12,7 @@ let
 in
 {
   services.fail2ban-cloudflare = {
-    enable = enabled;
+    enable = true;
     apiKeyFile = config.age.secrets.cloudflareFirewallApiKey.path;
     zoneId = "5a125e72bca5869bfb929db157d89d96";
 

8f8c23cffcdf1c16a9a324a8c524b4d2e6c6cbbd
Adrian Rangel <adrian.rangel@gmail.com>
2025-11-09 18:55:27 -0600

    add ssh pub key

--- modules/users/acrogenesis/default.nix
+++ modules/users/acrogenesis/default.nix
@@ -18,7 +18,7 @@
         "input"
       ];
       group = "acrogenesis";
-      openssh.authorizedKeys.keys = [ ];
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEiBWISainNZ2z111xO8t65x6LcMeP67BCtn1/OhvmsV adrian.rangel@gmail.com" ];
     };
     groups.acrogenesis.gid = 1000;
   };

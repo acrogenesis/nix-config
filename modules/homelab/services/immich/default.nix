073ce3748521dc45ff94e5b21b8dadb2e5da2e71
Adrian Rangel <adrian.rangel@gmail.com>
2025-11-21 06:32:05 -0600

    add hardware accel

--- modules/homelab/services/immich/default.nix
+++ modules/homelab/services/immich/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -71,6 +72,8 @@ in
       port = 2283;
       mediaLocation = "${cfg.mediaDir}";
       environment = {
+        IMMICH_FFMPEG_PATH = "${pkgs.ffmpeg-full}/bin/ffmpeg";
+        IMMICH_FFPROBE_PATH = "${pkgs.ffmpeg-full}/bin/ffprobe";
         IMMICH_FFMPEG_HWACCEL = "vaapi";
         IMMICH_FFMPEG_HWDEVICE = "/dev/dri/renderD128";
         LIBVA_DRIVER_NAME = "radeonsi";
